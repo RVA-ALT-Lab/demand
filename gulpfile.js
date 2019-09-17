d4b4845a3a0347c1c3b196cab7de48be72264fb6
Jeff Everhart <Jeffeverhart383@gmail.com>
2019-09-17 12:39:43 -0400

    use different version of vue

--- gulpfile.js
+++ gulpfile.js
@@ -122,7 +122,7 @@ gulp.task( 'browser-sync', function() {
 gulp.task( 'scripts', function() {
     var scripts = [
 
-        `${paths.node}/vue/dist/vue.min.js`,
+        `${paths.node}/vue/dist/vue.js`,
         // Start - All BS4 stuff
         `${paths.dev}/js/bootstrap4/bootstrap.bundle.js`,
 
